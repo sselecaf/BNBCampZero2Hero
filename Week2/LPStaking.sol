16cb43c31dd6224ac81d63487a3089fe5ff02466
sselecaf <65448755+sselecaf@users.noreply.github.com>
2023-03-18 15:42:07 +0800

    Add LP staking contract

---
+++ Week2/LPStaking.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IBEP20 {
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function totalSupply() external view returns (uint256);
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function mint(address account, uint amount) external;
+}
+
+contract LPStaking {
+    struct UserInfo {
+        uint256 amount;
+        uint256 rewardDebt;
+    }
+
+    struct PoolInfo {
+        IBEP20 lpToken;
+        uint256 allocPoint;
+        uint256 lastRewardBlock;
+        uint256 accRewardPerShare;
+    }
+
+    IBEP20 public rewardToken;
+    uint256 public rewardPerBlock;
+    uint256 public totalAllocPoint;
+    uint256 public startBlock;
+    uint256 public endBlock;
+
+    PoolInfo[] public poolInfo;
+    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(
+        IBEP20 _rewardToken,
+        uint256 _rewardPerBlock,
+        uint256 _startBlock,
+        uint256 _endBlock
+    ) {
+        rewardToken = _rewardToken;
+        rewardPerBlock = _rewardPerBlock;
+        startBlock = _startBlock;
+        endBlock = _endBlock;
+    }
+
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+
+    function addPool(uint256 _allocPoint, IBEP20 _lpToken) external {
+        for (uint256 pid = 0; pid < poolInfo.length; ++pid) {
+            require(poolInfo[pid].lpToken != _lpToken, "Pool already added");
+        }
+        totalAllocPoint += _allocPoint;
+        poolInfo.push(PoolInfo({
+            lpToken: _lpToken,
+            allocPoint: _allocPoint,
+            lastRewardBlock: block.number > startBlock ? block.number : startBlock,
+            accRewardPerShare: 0
+        }));
+    }
+
+    function setPool(uint256 _pid, uint256 _allocPoint) external {
+        totalAllocPoint = totalAllocPoint - poolInfo[_pid].allocPoint + _allocPoint;
+        poolInfo[_pid].allocPoint = _allocPoint;
+    }
+
+    function getPendingReward(address _user, uint256 _pid) public view returns (uint256) {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][_user];
+        uint256 accRewardPerShare = pool.accRewardPerShare;
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (block.number > pool.lastRewardBlock && lpSupply != 0) {
+            uint256 multiplier = block.number - pool.lastRewardBlock;
+            uint256 reward = multiplier * rewardPerBlock * pool.allocPoint / totalAllocPoint;
+            accRewardPerShare += reward / lpSupply;
+        }
+        return user.amount * accRewardPerShare - user.rewardDebt;
+    }
+
+    function updatePool(uint256 _pid) internal {
+        PoolInfo storage pool = poolInfo[_pid];
+        if (block.number <= pool.lastRewardBlock) {
+            return;
+        }
+        uint256 lpSupply = pool.lpToken.balanceOf(address(this));
+        if (lpSupply == 0) {
+            pool.lastRewardBlock = block.number;
+            return;
+        }
+        uint256 multiplier = block.number - pool.lastRewardBlock;
+        uint256 reward = multiplier * rewardPerBlock * pool.allocPoint / totalAllocPoint;
+        rewardToken.mint(address(this), reward);
+        pool.accRewardPerShare += reward / lpSupply;
+        pool.lastRewardBlock = block.number;
+    }
+
+    function deposit(uint256 _pid, uint256 _amount) external {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        updatePool(_pid);
+        if (user.amount > 0) {
+            uint256 pending = user.amount * pool.accRewardPerShare - user.rewardDebt;
+            rewardToken.transfer(msg.sender, pending);
+        }
+        pool.lpToken.transferFrom(msg.sender, address(this), _amount);
+        user.amount += _amount;
+        user.rewardDebt = user.amount * pool.accRewardPerShare;
+        emit Deposit(msg.sender, _pid, _amount);
+    }
+
+    function withdraw(uint256 _pid, uint256 _amount) external {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        require(user.amount >= _amount, "Withdraw: insufficient balance");
+        updatePool(_pid);
+        uint256 pending = user.amount * pool.accRewardPerShare - user.rewardDebt;
+        rewardToken.transfer(msg.sender, pending);
+        user.amount -= _amount;
+        user.rewardDebt = user.amount * pool.accRewardPerShare;
+        pool.lpToken.transfer(msg.sender, _amount);
+        emit Withdraw(msg.sender, _pid, _amount);
+    }
+
+    function emergencyWithdraw(uint256 _pid) external {
+        PoolInfo storage pool = poolInfo[_pid];
+        UserInfo storage user = userInfo[_pid][msg.sender];
+        pool.lpToken.transfer(msg.sender, user.amount);
+        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
+        user.amount = 0;
+        user.rewardDebt = 0;
+    }
+}
