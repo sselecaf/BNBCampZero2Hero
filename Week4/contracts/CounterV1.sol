acec3dba29aa76233dcc4c0ac158ec0909faecf1
sselecaf <65448755+sselecaf@users.noreply.github.com>
2023-04-01 23:57:31 +0800

    update Initializable.sol path

--- Week4/contracts/CounterV1.sol
+++ Week4/contracts/CounterV1.sol
@@ -2,7 +2,7 @@
 
 pragma solidity ^0.8.18;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/proxy/utils/Initializable.sol";
 
 contract CounterV1 is Initializable {
 
